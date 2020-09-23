a1a4fa931e9f7214ac1da9f0ce42660ce133e7b4
Kelj-menc <keljmenc@gmail.com>
2020-09-23 21:31:17 +0200

    nov sa poso jos uvek se jebem sa db

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
+    //ovo dole je malo probanja da update db - nisam zavrsio
+    protected $fillable = ['updated_at'];
+    //ovo dole je obrnuto tj protected
+    //protected $guarded = ['updated_at'];
     use HasFactory;
 }
